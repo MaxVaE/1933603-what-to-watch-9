6dc2a8a42ddafd5984a67f7faa304d77c4c1fd1e
Maksim Valiakine <mvaliakine@tourmalinecore.com>
2022-02-20 01:14:58 +0500

    module2-task1  - добавил компонент главной страницы  - добавил компоненты для всех страниц из markup

---
+++ project/src/components/welcome-main/welcome-main.tsx
@@ -0,0 +1,134 @@
+import FilmCard from '../film-card/film-card';
+import GenresItem from '../genres-item/genres-item';
+
+type WelcomeMainProps = {
+  geners: string[],
+  films: Array<{
+    name: string,
+    srcImg: string,
+  }>,
+  selectedFilm: {
+    title: string,
+    gener: string,
+    year: number,
+    srcPoster: string,
+  },
+};
+
+export default function WelcomeMain({
+  geners,
+  films,
+  selectedFilm,
+}: WelcomeMainProps): JSX.Element {
+  return (
+    <>
+      <section className="film-card">
+        <div className="film-card__bg">
+          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header film-card__head">
+          <div className="logo">
+            <a href="/" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <ul className="user-block">
+            <li className="user-block__item">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </li>
+            <li className="user-block__item">
+              <a href="/" className="user-block__link">Sign out</a>
+            </li>
+          </ul>
+        </header>
+
+        <div className="film-card__wrap">
+          <div className="film-card__info">
+            <div className="film-card__poster">
+              <img src={selectedFilm.srcPoster} alt={`${selectedFilm.title} poster`} width="218" height="327" />
+            </div>
+
+            <div className="film-card__desc">
+              <h2 className="film-card__title">{selectedFilm.title}</h2>
+              <p className="film-card__meta">
+                <span className="film-card__genre">{selectedFilm.gener}</span>
+                <span className="film-card__year">{selectedFilm.year}</span>
+              </p>
+
+              <div className="film-card__buttons">
+                <button className="btn btn--play film-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list film-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+          <ul className="catalog__genres-list">
+            {
+              geners.map((gener) => (
+                <GenresItem
+                  key={gener}
+                  gener={gener}
+                  isActive={gener === 'All genres'}
+                />
+              ))
+            }
+          </ul>
+
+          <div className="catalog__films-list">
+            {
+              films.map((film) => (
+                <FilmCard
+                  key={film.name}
+                  name={film.name}
+                  srcImg={film.srcImg}
+                />
+              ))
+            }
+          </div>
+
+          <div className="catalog__more">
+            <button className="catalog__button" type="button">Show more</button>
+          </div>
+        </section>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="/" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
