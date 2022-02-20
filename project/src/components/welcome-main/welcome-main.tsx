8747a5b742c9f98fea3640334a73c9c84575c801
Maksim Valiakine <mvaliakine@tourmalinecore.com>
2022-02-20 18:17:40 +0500

    module2-task1  - начал выносить footer и header

--- project/src/components/welcome-main/welcome-main.tsx
+++ project/src/components/welcome-main/welcome-main.tsx
@@ -1,5 +1,7 @@
 import FilmCard from '../film-card/film-card';
 import GenresItem from '../genres-item/genres-item';
+import Footer from '../footer/footer';
+import Header from '../header/header';
 
 type WelcomeMainProps = {
   geners: string[],
@@ -29,26 +31,7 @@ export default function WelcomeMain({
 
         <h1 className="visually-hidden">WTW</h1>
 
-        <header className="page-header film-card__head">
-          <div className="logo">
-            <a href="/" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <ul className="user-block">
-            <li className="user-block__item">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-              </div>
-            </li>
-            <li className="user-block__item">
-              <a href="/" className="user-block__link">Sign out</a>
-            </li>
-          </ul>
-        </header>
+        <Header />
 
         <div className="film-card__wrap">
           <div className="film-card__info">
@@ -115,19 +98,7 @@ export default function WelcomeMain({
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a href="/" className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   );
